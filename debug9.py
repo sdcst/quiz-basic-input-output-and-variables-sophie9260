5e91df78a8481c38fe46edfb5afb0f7cf18004d7
sophie9260 <158618434+sophie9260@users.noreply.github.com>
2024-04-12 12:44:36 -0700

    complete

--- debug9.py
+++ debug9.py
@@ -3,6 +3,8 @@
 Debug this program so that it runs
 """
 x = input("Enter a number")
+x = float(x)
 y = 2
+y = float(y)
 answer = x+y
-print(The answer is + answer)
+print(f"The answer is {answer}")
